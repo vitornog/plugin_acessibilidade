f669a7bc250526ee650277d8ddd7e3f6b78a9a63
Vitor Hugo Nogueira <5271@inb.gov.br>
2022-04-01 08:46:48 -0300

    Correções no menu flutuante

--- src/acessibilidade.js
+++ src/acessibilidade.js
@@ -1,48 +1,56 @@
 let valor = 0.9;
 
-const acessibilidade = (tipo) => {
+function acessibilidade(tipo){
 	
 	resetAcessibilidade();
 
 	if(tipo=="cinza"){
 		$("html").addClass('escala-cinza');
 	}
-	else if(tipo=='aumentar'){
-		valor+=0.1;
-        $("*:not(h1,svg,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link),div:not(.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link),a:not(.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link)").css('font-size',valor+'rem');
-	}
+    else if (tipo == 'aumentar') {
+        valor += 0.1;
+        $("*:not(.navbar-fixed-top,h1,svg,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link,#plugin-acessibilidade-toolbar,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-inner,.plugin-acessibilidade-toolbar-inner p,.plugin-acessibilidade-toolbar-items, .plugin-acessibilidade-toolbar-item a, .plugin-acessibilidade-toolbar-item a span),div:not(.navbar,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link,#plugin-acessibilidade-toolbar),a:not(.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link)").css('font-size', valor + 'rem');
+        $(".navbar-fixed-top").find("*").css('font-size', '14px');
+        $("#plugin-acessibilidade-toolbar,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-inner p").css('font-size', '16px');
+        $(".plugin-acessibilidade-toolbar-items,.plugin-acessibilidade-toolbar-item a, .plugin-acessibilidade-toolbar-item a span").css('font-size', '14px');
+    }
 	else if(tipo=='diminuir'){
 		if(valor>0.9){
 			valor-=0.1;
-            $("*:not(h1,svg,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link), div:not(.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link),a:not(.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link)").css('font-size',valor+'rem');
-		}
+            $("*:not(.navbar-fixed-top,h1,svg,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link,#plugin-acessibilidade-toolbar,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-inner,.plugin-acessibilidade-toolbar-inner p, .plugin-acessibilidade-toolbar-items, .plugin-acessibilidade-toolbar-item a, .plugin-acessibilidade-toolbar-item a span),div:not(.navbar,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link,#plugin-acessibilidade-toolbar),a:not(.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link)").css('font-size', valor + 'rem');
+        }
+        $(".navbar-fixed-top").find("*").css('font-size', '14px');
+        $("#plugin-acessibilidade-toolbar,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-inner p").css('font-size', '16px');
+        $(".plugin-acessibilidade-toolbar-items,.plugin-acessibilidade-toolbar-item a, .plugin-acessibilidade-toolbar-item a span").css('font-size', '14px');
 	}
 	else if(tipo=="preto"){
-		$("*:not(img)").addClass('fundo-preto');
+        $("*:not(img,.IcoInbNet)").addClass('fundo-preto');
 	}
 	else if(tipo=="branco"){
-		$("*:not(img)").addClass('fundo-claro');
-		$("img").addClass('fundo-imagens');
+        $("*:not(img,.IcoInbNet)").addClass('fundo-claro');
+        $("img:not(.IcoInbNet)").addClass('fundo-imagens');
 	}
 	else if(tipo=="alto-contraste"){
 		$("html").addClass('alto-contraste');
 	}
 }
 
-const resetAcessibilidade = (tipo) =>{
+function resetAcessibilidade(tipo){
 	$("*").removeClass('fundo-claro').removeClass("fundo-preto").removeClass("cinza").removeClass("alto-contraste");
 	$("body,html").removeClass('escala-cinza');
 	$("img").removeClass('fundo-imagens');
 	if(tipo=="all"){
-        $("*:not(h1,svg,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link),div:not(.plugin-acessibilidade-toolbar-toggle)").css('font-size','1.0rem');
+        $("*:not(.navbar-fixed-top,h1,svg,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link,#plugin-acessibilidade-toolbar,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-inner,.plugin-acessibilidade-toolbar-inner p, .plugin-acessibilidade-toolbar-items, .plugin-acessibilidade-toolbar-item a, .plugin-acessibilidade-toolbar-item a span),div:not(.navbar,.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link,#plugin-acessibilidade-toolbar),a:not(.plugin-acessibilidade-toolbar-toggle,.plugin-acessibilidade-toolbar-link,.plugin-acessibilidade-toolbar-toggle-link)").css('font-size', '0.9rem');
+        $(".navbar-fixed-top").find("*").css('font-size', '14px');        
+        $("#plugin-acessibilidade-toolbar,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-overlay,.plugin-acessibilidade-toolbar-inner p").css('font-size', '16px');
+        $(".plugin-acessibilidade-toolbar-items,.plugin-acessibilidade-toolbar-item a, .plugin-acessibilidade-toolbar-item a span").css('font-size', '14px');
 	}
-	$(".plugin-acessibilidade-toolbar-toggle").css("font-size","1em");
+	$(".plugin-acessibilidade-toolbar-toggle").css("font-size","0.9em");
 }
 
 $(function() {
 	$(".plugin-acessibilidade-toolbar-toggle a").on("click",function(){
 		$("#plugin-acessibilidade-toolbar").toggleClass("plugin-acessibilidade-toolbar-close").toggleClass("plugin-acessibilidade-toolbar-open");
-		console.log('ok');
 	});
 
 	$(".plugin-acessibilidade-tools .plugin-acessibilidade-btn-resize-plus").on("click",function(){
